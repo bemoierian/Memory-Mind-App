8268eef8a65d80413690094929fdd0b7c1fce6fb
Bemoi Erian <bemoi.erian@gmail.com>
2023-06-18 16:41:20 +0300

    added name and used storage in user model

--- lib/data/models/signin_res_model.dart
+++ lib/data/models/signin_res_model.dart
@@ -1,13 +1,18 @@
 class SignInResModel {
   String? token;
   String? userId;
+  String? name;
+  double? usedStorage;
   String? message;
 
-  SignInResModel({this.token, this.userId});
+  SignInResModel(
+      {this.token, this.userId, this.name, this.usedStorage, this.message});
 
   SignInResModel.fromJson(Map<String, dynamic> json) {
     token = json['token'];
     userId = json['userId'];
+    name = json['name'];
+    usedStorage = json['usedStorage'];
     message = json['message'];
   }
 
@@ -16,6 +21,8 @@ class SignInResModel {
     data['token'] = this.token;
     data['userId'] = this.userId;
     data['message'] = this.message;
+    data['name'] = this.name;
+    data['usedStorage'] = this.usedStorage;
     return data;
   }
 }
